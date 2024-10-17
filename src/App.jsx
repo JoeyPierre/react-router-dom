7d0b708b84b0785c7b6dac2222f907c08f9b8ec9
Joey Pierre <JoeyPierre@joeypierre27@gmail.com>
2024-10-17 14:22:07 -0400

    98%

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import PokemonList from './components/PokemonList';
 import NavBar from './components/NavBar';
 import { Route, Routes } from 'react-router-dom';
 import PokemonDetails from './components/PokemonDetails';
-
+import PokemonForm from './components/PokemonForm';
 const initialState = [
   { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
   { _id: 2, name: 'ivysaur', weight: 130, height: 10 },
@@ -11,10 +11,19 @@ const initialState = [
   { _id: 4, name: 'charmander', weight: 85, height: 6 },
   { _id: 5, name: 'charmeleon', weight: 190, height: 11 },
 ];
-
 const App = () => {
   const [pokemon, setPokemon] = useState(initialState);
-  return (
+const addPokemon = (newPokemonData) => {
+  newPokemonData._id = pokemon.length + 1;
+  setPokemon([...pokemon, newPokemonData]);
+const handleSubmit = (e) => {
+  e.preventDefault();
+  props.addPokemon(formData);
+  setFormData(initialState);
+};
+
+};
+return (
     <>
     <NavBar />
     <h1>Pokemon!</h1>
@@ -25,6 +34,7 @@ const App = () => {
     path="/pokemon/:pokemonId"
     element={<PokemonDetails pokemon={pokemon} />}
   />
+  <Route path="/pokemon/new" element={<PokemonForm addPokemon={addPokemon} />} />
 </Routes>
 
     </>
